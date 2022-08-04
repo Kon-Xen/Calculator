195c56fba71b1f60b774c6edcbb113538f1a9fc0
Konstantinos <kon.xen@gmail.com>
2022-08-04 15:58:12 +0100

    Basic structure laid out.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,108 @@
+namespace Calculator;
+
+public class Calculator
+{
+    public string opperator ;
+    public string[] numbers;
+    public string ResultMessage = " ";
+    public int IntResult;
+    public decimal DecimalResult;
+     public void ShowMessage( string message ) 
+    {
+        Console.WriteLine( message );
+    }
+    
+     public void GetOperator()
+     {
+        while ( opperator == null)
+        {
+            Console.WriteLine( "Choose operation (+,-,*,/): " );
+            var input = Console.ReadLine();
+            opperator = CheckOperatorIsValid( input ) ? input : null;   
+        }
+    }
+     
+     public void GetNumbers()
+     {
+         var finished = false;
+         while ( finished == false)
+         {
+             Console.WriteLine( "enter numbers (seperated with space): " );
+             var input = Console.ReadLine();
+             var InputNumbers = input.Split(" ");
+
+             foreach (var number in InputNumbers)
+             {
+                 if (!CheckInputIsValid(number))
+                 {
+                     Console.WriteLine( "Not all numbers are valid. Did you use space ? " );
+                     break;
+                 }
+                 numbers = InputNumbers;
+                 finished = true;
+             }
+         }
+    }
+     
+     static bool CheckOperatorIsValid( string input )
+     {
+         Console.WriteLine( "did you enter a valid operator ? " );
+         return input is "+" or "-" or "*" or "/";
+     } 
+
+    static bool CheckInputIsValid( string input )
+    {
+        return int.TryParse(input, out var value1) || decimal.TryParse(input, out var value2);
+    }
+
+    public static void PrepareResultMessage()
+    {
+        static string numberString = "yo";
+        foreach (var number in numbers)
+        {
+            numberString.Format( number + opperator);
+        }
+    }
+
+    public void DoOpperation()
+    {
+        switch ( opperator )
+        {
+            case "+":
+               Addition( numbers );
+                break;
+            case "-":
+                ResultMessage = 
+                Subtraction( numbers);
+                break;
+            case "*":
+                Multiplication( numbers );
+                break;
+            case "/":
+                Division(numbers);
+                break;
+        }
+    } 
+
+    public void ShowResult()
+    {
+        
+    }
+
+    public void Addition( string[] numbers )
+    {
+        // return numbers.Sum();
+    }
+    public void Subtraction( string[] numbers )
+    {
+        
+    }
+    public void Multiplication( string[] numbers )
+    {
+        
+    }
+    public void Division( string[] numbers )
+    {
+        Console.WriteLine("");
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// CLI Calculator by KX
+
+using System.Diagnostics.CodeAnalysis;
+using System.Net.NetworkInformation;
+using Calculator;
+
+var calc = new Calculator.Calculator();
+
+calc.ShowMessage( "Calculator MK1" );
+calc.ShowMessage( "Please use spaces between numbers / operators" );
+
+calc.GetOperator();
+calc.GetNumbers();
+calc.DoOpperation();
+calc.ShowResult();
+
+//Debug
+// foreach (var number in calc.numbers)
+// {
+//     Console.WriteLine( number );
+// }
